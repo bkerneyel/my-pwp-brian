85cad6390881068be390f555f4a6f4498f3f081d
Brian Kerney-El <bkerneyel@cnm.edu>
2021-11-13 10:33:58 -0700

    got it interactive!!!

--- public_html/script.js
+++ public_html/script.js
@@ -1,2 +1,9 @@
-
+function download(url) {
+    const a = document.createElement('a')
+    a.href = url
+    a.download = url.split('/').pop()
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+}
 
